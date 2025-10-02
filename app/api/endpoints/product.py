979fd5b6295b474203b830f7472988d577242e9f
Maxim Pokrovsky <manager72@star-vector.ru>
2025-10-02 16:11:32 +0300

    Реализованы энпоинты для crud с products.

--- app/api/endpoints/product.py
+++ app/api/endpoints/product.py
@@ -1,11 +1,10 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, Query
+from fastapi import APIRouter, Depends, Query, status, HTTPException
 
 from app.dependencies.product import get_product_service
-from app.domain.models import SubjectDataWithProductsResponse
+from app.domain.models import ResponseMessage, CreateProduct, ProductResponse, SubjectDataWithProductsResponse
 from app.service.product import ProductService
-
 
 router = APIRouter(prefix="/products", tags=["Products"])
 
@@ -19,4 +18,54 @@ async def get_products_grouped_by_subjects(
     return await service.get_products_grouped_by_subjects(
         limit=limit,
         offset=offset,
+    )
+
+
+@router.get("/{product_id}", status_code=status.HTTP_200_OK)
+async def get_product(
+    product_id: str,
+    service: Annotated[ProductService, Depends(get_product_service)],
+) -> ProductResponse:
+    product = await service.get_product(product_id)
+
+    if not product:
+        raise HTTPException(status_code=404, detail="Product data not found")
+
+    return product
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+async def create_product(
+    data: CreateProduct,
+    service: Annotated[ProductService, Depends(get_product_service)],
+) -> ProductResponse:
+    product = await service.create_product(data)
+
+    return product
+
+
+@router.put("/{product_id}", status_code=status.HTTP_200_OK)
+async def update_product(
+    product_id: str,
+    data: CreateProduct,
+    service: Annotated[ProductService, Depends(get_product_service)],
+) -> ProductResponse:
+    product = await service.update_product(
+        product_id=product_id,
+        data=data,
+    )
+
+    return product
+
+
+@router.delete("/{product_id}", status_code=status.HTTP_200_OK)
+async def delete_product(
+    product_id: str,
+    service: Annotated[ProductService, Depends(get_product_service)],
+) -> ResponseMessage:
+    await service.delete(product_id)
+
+    return ResponseMessage(
+        status=status.HTTP_200_OK,
+        message="Product was deleted",
     )
