d68e063a90279e4fcb44573d7374dcaadbd00b75
Maxim Pokrovsky <manager72@star-vector.ru>
2025-11-11 17:44:34 +0300

    [*] Обновил логику кеширования

--- app/infrastructure/cache.py
+++ app/infrastructure/cache.py
@@ -1,3 +1,4 @@
+import asyncio
 import hashlib
 import json
 import logging
@@ -6,6 +7,7 @@ from functools import wraps
 import inspect
 
 from redis.asyncio import Redis
+from fastapi import HTTPException, status
 
 from app.infrastructure.redis_client import redis_client
 
@@ -16,6 +18,8 @@ logger = logging.getLogger(__name__)
 def redis_cache_async(
     redis_instance: Redis = redis_client,
     ttl: int = 60,
+    lock_ttl: int = 70,
+    wait_for_result_ttl: int = 75,
     key_prefix: str = "cache",
     exclude_args=None,
 ):
@@ -43,7 +47,7 @@ def redis_cache_async(
 
 
             key_parts = [func.__module__, func.__qualname__, str(filtered_args)]
-            cache_key = f"{key_prefix}:{hashlib.md5(':'.join(key_parts).encode()).hexdigest()}"
+            cache_key = f"{key_prefix}:{hashlib.md5(":".join(key_parts).encode()).hexdigest()}"
 
             try:
                 cached_result = await redis_instance.get(cache_key)
@@ -52,15 +56,44 @@ def redis_cache_async(
                     return json.loads(cached_result.decode("utf-8"))
             except Exception as e:
                 logger.warning(f"Ошибка получения кэша для ключа '{cache_key}': {e}")
+            
+            lock_key = f"lock:{cache_key}"
+            lock = redis_instance.lock(lock_key, timeout=lock_ttl)
 
-            result = await func(*args, **kwargs)
+            acquired = await lock.acquire(blocking=False)
 
-            try:
-                await redis_instance.setex(cache_key, ttl, json.dumps(result, ensure_ascii=False))
-            except Exception as e:
-                logger.warning(f"Ошибка установки кэша для ключа '{cache_key}': {e}")
+            if acquired:
+                try:
+                    logger.info(f"Получен Lock для ключа for key: {lock_key}, выполнение '{func.__qualname__}'")
+                    result = await func(*args, **kwargs)
 
-            return result
+                    await redis_instance.setex(cache_key, ttl, json.dumps(result, ensure_ascii=False))
+                    logger.info(f"Записан кеш для '{func.__qualname__}', key: {cache_key}, ttl: {ttl}")
+
+                    return result
+                except Exception as e:
+                    logger.warning(f"Ошибка установки кэша для ключа '{cache_key}': {e}")
+                finally:
+                    await lock.release()
+                    logger.info(f"Освобождаем Lock: {lock_key}")
+            else:
+                # Блокировка занята. Ждём освобождения и пробуем получить из кэша
+                logger.info(f"Блокировка занята: {lock_key}, ожидание результата из кэша...")
+
+                for _ in range(int(wait_for_result_ttl / 0.5)): # Проверяем кэш каждые 0.5 сек
+                    await asyncio.sleep(0.5)
+
+                    cached_result = await redis_instance.get(cache_key)
+
+                    if cached_result:
+                        logger.info(f"Получение данных из кэша после ожидания блокировки, key: {cache_key}")
+                        return json.loads(cached_result.decode("utf-8"))
+                
+                logger.error(f"Истекло время ожидания результата после разблокировки ключа: {lock_key}")
+                raise HTTPException(
+                    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                    detail="Timed out waiting for report generation by another request."
+                )
 
         return wrapper
 
