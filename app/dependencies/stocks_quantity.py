cbe847a4f235ffce809c01881d8bc77c18fa0de2
Maxim Pokrovsky <manager72@star-vector.ru>
2025-11-29 12:59:48 +0300

    [*] Переписал проверку баркодов и статусов при попытке изменить остатки.

--- app/dependencies/stocks_quantity.py
+++ app/dependencies/stocks_quantity.py
@@ -38,36 +38,51 @@ async def validate_edit_quantity_data(
     card_status_service: CardStatusService = Depends(get_card_status_service),
     card_data_service: CardDataService = Depends(get_card_data_service),
 ) -> EditQuantityValidationResult:
-    all_barcodes = {
-        item.sku
-        for account_data in edit_data.values()
-        for item in account_data.stocks
-    }
+    all_barcodes: dict[set[str]] = {}
+
+    for acc, skus_data in edit_data.items():
+        skus = {item.sku for item in skus_data.stocks}
+
+        if not skus:
+            continue
+
+        if not acc in all_barcodes:
+            all_barcodes[acc] = set()
+
+        all_barcodes[acc].update(skus)
 
     if not all_barcodes:
         return EditQuantityValidationResult(allowed={}, invalid={}, closed_with_nonzero={})
 
+    barcodes_nm_ids = await card_data_service.get_article_ids_by_barcodes(all_barcodes)
 
-    barcode_to_nm = await card_data_service.get_article_ids_by_barcodes(list(all_barcodes))
+    all_nm_ids = set()
 
-    nm_ids = list(set(barcode_to_nm.values()))
-    nm_to_status = await card_status_service.get_status_by_nm_ids(nm_ids) if nm_ids else {}
+    for _, barcodes in barcodes_nm_ids.items():
+        all_nm_ids.update(barcodes.values())
+
+    cards_statuses = await card_status_service.get_status_by_nm_ids(list(all_nm_ids)) if all_nm_ids else {}
 
     allowed_items_by_account = defaultdict(list)
     invalid_barcodes_by_account = defaultdict(list)
     closed_with_nonzero_by_account = defaultdict(list)
 
     for account, account_data in edit_data.items():
+        acc_nm_ids = barcodes_nm_ids.get(acc)
+
+        if not acc_nm_ids:
+            continue
+
         for item in account_data.stocks:
             barcode = item.sku
-            nm_id = barcode_to_nm.get(barcode)
+            nm_id = acc_nm_ids.get(barcode)
 
             if nm_id is None:
                 invalid_barcodes_by_account[account].append(barcode)
                 continue
 
-            status = nm_to_status.get(nm_id, "active")
-            is_closed = status in {CardStatusEnum.closed, CardStatusEnum.closing_pending}
+            status = cards_statuses.get(nm_id, "active")
+            is_closed = status != CardStatusEnum.active
 
             if not is_closed or item.amount == 0:
                 allowed_items_by_account[account].append(item)
@@ -81,6 +96,6 @@ async def validate_edit_quantity_data(
 
     return EditQuantityValidationResult(
         allowed=allowed,
-        invalid=dict(invalid_barcodes_by_account),
-        closed_with_nonzero=dict(closed_with_nonzero_by_account),
+        invalid=invalid_barcodes_by_account,
+        closed_with_nonzero=closed_with_nonzero_by_account,
     )
