93487950fe1215ab1cc9e042d6baced5a7aa0156
AKhr1st <khristolubov_ad@star-vector.ru>
2026-04-13 15:59:47 +0300

    задача 12599. добавил поле для отзывов в карточку. скорректировал запрос расчета рейтинга для расчета количества отзывов

--- app/repository/article.py
+++ app/repository/article.py
@@ -32,6 +32,12 @@ class ArticleRepository:
                     FROM cost_price
                 ) t
                 WHERE rn = 1
+            ), FeedbackCounts AS (
+                SELECT
+                    nmid AS article_id,
+                    COUNT(*) AS reviews_count
+                FROM wb_feedbacks
+                GROUP BY nmid
             )
             SELECT
                 a.account,
@@ -49,6 +55,7 @@ class ArticleRepository:
                 cd.height,
                 cd.barcode,
                 cd.rating,
+                COALESCE(fc.reviews_count, 0) AS reviews_count,
                 cd.manager,
                 cd.local_card_name,
                 cd.chrt_id,
@@ -67,7 +74,9 @@ class ArticleRepository:
                 current_real_fbs_stocks_qty crfs
                 ON a.local_vendor_code = crfs.local_vendor_code
             LEFT JOIN product_notes pn
-            	on a.nm_id = pn.nm_id and a.account = pn.account and a.local_vendor_code = pn.local_vendor_code;
+            	on a.nm_id = pn.nm_id and a.account = pn.account and a.local_vendor_code = pn.local_vendor_code
+            LEFT JOIN FeedbackCounts fc
+                ON fc.article_id = a.nm_id;
                             """
             rows = await conn.fetch(query)
             result = []
